c8c803f3b3a5dcb6f72ebbb7a0133d4e6f65fde1
Fabian Mittermair <fam@solute.de>
2021-12-02 10:02:04 +0100

    hpebladesystem: Fix broken splitting

--- lib/oxidized/model/hpebladesystem.rb
+++ lib/oxidized/model/hpebladesystem.rb
@@ -11,7 +11,7 @@ class HPEBladeSystem < Oxidized::Model
 
   cmd :all do |cfg|
     cfg = cfg.delete("\r").each_line.to_a[0..-1].map { |line| line.rstrip }.join("\n") + "\n"
-    cfg.cut_tail
+    cfg.each_line.to_a[0..-2].join
   end
 
   cmd :secret do |cfg|
